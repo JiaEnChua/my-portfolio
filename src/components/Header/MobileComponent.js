f89e924ff082bbfb9b17930c493a0a07578f929c
jiaenchua <jiaen_je@hotmail.com>
2023-05-18 23:47:03 -0700

    rewrite header icon

--- src/components/Header/MobileComponent.js
+++ src/components/Header/MobileComponent.js
@@ -1,65 +1,45 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import React, { useState } from 'react';
+import { scroller } from 'react-scroll';
 import map from 'lodash/map';
-import { Link } from 'react-scroll';
-import { Menu as MenuIcon } from '@mui/icons-material';
+import Button from 'react-bootstrap/Button';
 
 import { PAGE_ITEMS } from '../constants';
 
-export default function MobileComponent() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
+function MobileComponent() {
+  const [isPanelOpen, setPanelOpen] = useState(false);
+
+  const scrollToSection = (section) => {
+    scroller.scrollTo(section, {
+      duration: 800,
+      smooth: 'easeInOutQuart',
+    });
+    togglePanel();
   };
 
-  return (
-    <div
-      style={{
-        height: '50px',
-        position: 'sticky',
-        top: '0',
-        background: 'white',
-        zIndex: 1000,
-      }}
-    >
-      <Button
-        id='basic-button'
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup='true'
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-      >
-        <MenuIcon />
-      </Button>
-      <Menu
-        id='basic-menu'
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
+  const togglePanel = () => {
+    setPanelOpen(!isPanelOpen);
+  };
+
+  const renderPanel = () => (
+    <div className='panel'>
+      <ul className='panel-items'>
         {map(PAGE_ITEMS, (eachItem) => (
-          <Link
-            activeClass='active'
-            to={eachItem.to}
-            spy={true}
-            smooth={true}
-            offset={eachItem.offset}
-            duration={500}
-            key={eachItem.label}
-          >
-            <MenuItem onClick={handleClose}>{eachItem.label}</MenuItem>
-          </Link>
+          <li key={eachItem.label}>
+            <p onClick={() => scrollToSection(eachItem.to)}>{eachItem.label}</p>
+          </li>
         ))}
-      </Menu>
+      </ul>
+    </div>
+  );
+
+  return (
+    <div className='navigation-icon'>
+      <Button variant='primary' onClick={togglePanel}>
+        Navigation
+      </Button>
+      {isPanelOpen && renderPanel()}
     </div>
   );
 }
+
+export default MobileComponent;
